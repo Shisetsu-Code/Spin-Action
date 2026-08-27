9d22935643f17e5e87fe47b5c8191cc2961ccfb6
Shisetsu <55253798+Shisetsu-Code@users.noreply.github.com>
2026-08-27 15:00:04 -0300

    Use streaming Pragmatic provider in the GUI

--- tester_spin/providers/__init__.py
+++ tester_spin/providers/__init__.py
@@ -1,4 +1,4 @@
 from tester_spin.providers.base import ProviderAdapter, ProviderRegistry
-from tester_spin.providers.pragmatic import PragmaticProvider
+from tester_spin.providers.pragmatic_live import PragmaticProvider
 
 __all__ = ["ProviderAdapter", "ProviderRegistry", "PragmaticProvider"]
