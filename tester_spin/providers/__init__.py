d6f7fb455b14400d34ccdb3aa2258dd5a9b3a4bd
Shisetsu <55253798+Shisetsu-Code@users.noreply.github.com>
2026-08-27 14:52:24 -0300

    Add provider package initializer

---
+++ tester_spin/providers/__init__.py
@@ -0,0 +1,4 @@
+from tester_spin.providers.base import ProviderAdapter, ProviderRegistry
+from tester_spin.providers.pragmatic import PragmaticProvider
+
+__all__ = ["ProviderAdapter", "ProviderRegistry", "PragmaticProvider"]
