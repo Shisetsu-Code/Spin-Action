3682ee43660ea1ec93270d90eb76a2aa09098b07
Shisetsu <55253798+Shisetsu-Code@users.noreply.github.com>
2026-09-07 18:13:06 -0300

    Register Belatra provider export

--- tester_spin/providers/__init__.py
+++ tester_spin/providers/__init__.py
@@ -1,4 +1,10 @@
 from tester_spin.providers.base import ProviderAdapter, ProviderRegistry
+from tester_spin.providers.belatra import BelatraProvider
 from tester_spin.providers.pragmatic_hybrid import PragmaticProvider
 
-__all__ = ["ProviderAdapter", "ProviderRegistry", "PragmaticProvider"]
+__all__ = [
+    "ProviderAdapter",
+    "ProviderRegistry",
+    "PragmaticProvider",
+    "BelatraProvider",
+]
