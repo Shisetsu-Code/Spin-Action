7d5a09ab7ccf8a7f21f94954823203efe960d670
Shisetsu <55253798+Shisetsu-Code@users.noreply.github.com>
2026-09-10 17:51:57 -0300

    Register RubyPlay provider export

--- tester_spin/providers/__init__.py
+++ tester_spin/providers/__init__.py
@@ -3,6 +3,7 @@ from tester_spin.providers.bgaming import BGamingProvider
 from tester_spin.providers.belatra import BelatraProvider
 from tester_spin.providers.one_spin4win import OneSpin4WinProvider
 from tester_spin.providers.pragmatic_hybrid import PragmaticProvider
+from tester_spin.providers.rubyplay import RubyPlayProvider
 
 __all__ = [
     "ProviderAdapter",
@@ -11,4 +12,5 @@ __all__ = [
     "OneSpin4WinProvider",
     "BelatraProvider",
     "BGamingProvider",
+    "RubyPlayProvider",
 ]
