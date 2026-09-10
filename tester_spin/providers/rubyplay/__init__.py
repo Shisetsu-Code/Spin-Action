0250008686c219f464c0cb0dcd86a159d52e0ef3
Shisetsu <55253798+Shisetsu-Code@users.noreply.github.com>
2026-09-10 19:29:20 -0300

    Install semantic RubyPlay launcher parser

--- tester_spin/providers/rubyplay/__init__.py
+++ tester_spin/providers/rubyplay/__init__.py
@@ -1,3 +1,9 @@
+from tester_spin.providers.rubyplay.launcher_params import (
+    install_runtime_launcher_parser,
+)
+
+install_runtime_launcher_parser()
+
 from tester_spin.providers.rubyplay.adapter import RubyPlayProvider
 
 __all__ = ["RubyPlayProvider"]
