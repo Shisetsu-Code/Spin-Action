a871819b725aa943f6666c1f4289cbf4d7417972
Shisetsu <55253798+Shisetsu-Code@users.noreply.github.com>
2026-09-11 03:28:43 -0300

    Export isolated Red Tiger provider

--- tester_spin/providers/__init__.py
+++ tester_spin/providers/__init__.py
@@ -3,6 +3,7 @@ from tester_spin.providers.bgaming import BGamingProvider
 from tester_spin.providers.belatra import BelatraProvider
 from tester_spin.providers.one_spin4win import OneSpin4WinProvider
 from tester_spin.providers.pragmatic_hybrid import PragmaticProvider
+from tester_spin.providers.redtiger import RedTigerProvider
 from tester_spin.providers.rubyplay import RubyPlayProvider
 
 __all__ = [
@@ -13,4 +14,5 @@ __all__ = [
     "BelatraProvider",
     "BGamingProvider",
     "RubyPlayProvider",
+    "RedTigerProvider",
 ]
