8e901046eef6f0d10304fa10c9763de348d5ea62
Shisetsu <55253798+Shisetsu-Code@users.noreply.github.com>
2026-09-10 23:44:59 -0300

    Install RubyPlay bootstrap fallback

--- tester_spin/providers/rubyplay/__init__.py
+++ tester_spin/providers/rubyplay/__init__.py
@@ -10,7 +10,9 @@ install_runtime_launcher_resolver()
 
 from tester_spin.providers.rubyplay.adapter import RubyPlayProvider
 from tester_spin.providers.rubyplay.runtime_contracts import install_runtime_contracts
+from tester_spin.providers.rubyplay.bootstrap_fallback import install_bootstrap_fallback
 
 install_runtime_contracts()
+install_bootstrap_fallback()
 
 __all__ = ["RubyPlayProvider"]
