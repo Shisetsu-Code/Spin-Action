cc8165cd46cf0e9ed88d47483a5d2195fb5dd775
Shisetsu <55253798+Shisetsu-Code@users.noreply.github.com>
2026-09-10 23:46:54 -0300

    Install RubyPlay availability classification

--- tester_spin/providers/rubyplay/__init__.py
+++ tester_spin/providers/rubyplay/__init__.py
@@ -11,8 +11,10 @@ install_runtime_launcher_resolver()
 from tester_spin.providers.rubyplay.adapter import RubyPlayProvider
 from tester_spin.providers.rubyplay.runtime_contracts import install_runtime_contracts
 from tester_spin.providers.rubyplay.bootstrap_fallback import install_bootstrap_fallback
+from tester_spin.providers.rubyplay.availability import install_availability_classification
 
 install_runtime_contracts()
 install_bootstrap_fallback()
+install_availability_classification()
 
 __all__ = ["RubyPlayProvider"]
