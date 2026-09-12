0e34e4ef9b54fe0644dc70356405f362b9bd3a33
Shisetsu <55253798+Shisetsu-Code@users.noreply.github.com>
2026-09-12 16:38:21 -0300

    Use exhaustive provider wrappers

--- tester_spin/providers/__init__.py
+++ tester_spin/providers/__init__.py
@@ -1,10 +1,10 @@
 from tester_spin.providers.base import ProviderAdapter, ProviderRegistry
-from tester_spin.providers.bgaming import BGamingProvider
-from tester_spin.providers.belatra import BelatraProvider
-from tester_spin.providers.one_spin4win import OneSpin4WinProvider
+from tester_spin.providers.bgaming_exhaustive import BGamingProvider
+from tester_spin.providers.belatra_exhaustive import BelatraProvider
+from tester_spin.providers.one_spin4win_exhaustive import OneSpin4WinProvider
 from tester_spin.providers.pragmatic_hybrid import PragmaticProvider
 from tester_spin.providers.redtiger import RedTigerProvider
-from tester_spin.providers.rubyplay import RubyPlayProvider
+from tester_spin.providers.rubyplay.exhaustive import RubyPlayProvider
 
 __all__ = [
     "ProviderAdapter",
